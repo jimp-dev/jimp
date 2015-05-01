68c109146c5f18b1d0587c3509071e86dcf00afa
Oliver Moran <oliver.moran@gmail.com>
2015-05-02 00:40:51 +0100

    completed test of chained events

--- test/test.js
+++ test/test.js
@@ -2,12 +2,71 @@ var Jimp = require("../jimp.js");
 
 // open a file called "lenna.png"
 var lenna = new Jimp("lenna.png", function () {
-    this.resize(220, 220) // resize
-        .write("./output/lenna-small.png") // save
-        .quality(60) // set JPEG quality
-        .write("./output/lenna-small.jpg") // save as JPEG
-        .greyscale() // set greyscale
-        .write("./output/lena-small-bw.png") // save again
-        .crop(80, 100, 80, 50) // crop
-        .write("./output/lena-small-bw-cropped.png"); // save again
+    this.write("./output/lenna-copy.png") // PNG copy
+        .write("./output/lenna-copy.jpg") // JPEG copy
+        .quality(10).write("./output/lenna-copy-low.jpg").quality(100) // JPEG copy at 10 quality
+        .invert().write("./output/lenna-invert.png").invert() // invert
+        .rotate(90).write("./output/lenna-rotate.png").rotate(-90) // rotate
+        .flip(true, false).write("./output/lenna-flip-horizontal.png").flip(true, false) // flip horizontal
+        .flip(false, true).write("./output/lenna-flip-vertical.png").flip(false, true) // flip vertical
+
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.greyscale().write("./output/lena-greyscale.png") // black and white
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.blur(5).write("./output/lena-blur.png") // blur
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.gaussian(5).write("./output/lena-gaussian.png") // gaussian
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.resize(64, 64).write("./output/lenna-resized.png") // resize
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.sepia().write("./output/lenna-sepia.png") // sepia
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_JPEG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.opacity(0.5).write("./output/lenna-opacity.png") // opacity
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_JPEG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.crop(128, 192, 256, 128).write("./output/lenna-cropped.png") // opacity
+        });
+    }); 
+    
+    this.getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+        if (err) throw err;
+        var img = new Jimp(buffer, function () {
+            this.scale(0.5).write("./output/lenna-scale.png") // scale
+            this.blit(lenna.greyscale(), lenna.bitmap.width - this.bitmap.width, 0);
+            lenna.write("./output/lenna-blitted.png")
+        });
+    });
+
+
 });
