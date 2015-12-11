289444f03f472b618869feb9afbc31c4cdd4c896
Phil <phil@phil-seaton.com>
2015-12-10 21:48:41 -0800

    Adds a comment

--- browserify-build.sh
+++ browserify-build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-NODE_ENV=BROWSER browserify -t envify -t uglifyify index.js | uglifyjs --compress --mangle > browser/jimp.js
+# This mechanism could offer simple variations on the build.
+# Features could be productively grouped for smaller file size
+# eg: I/O, Affine Transforms, Bitmap Operations, Gamma Curves, and Layers
+# Initial Build includes only functionality useful for thumbnailing:
+# Resize, (auto)rotate, mime, encoding/decoding jpg/png
+ENVIRONMENT=BROWSER \
+browserify -t envify -t uglifyify index.js | uglifyjs --compress --mangle > browser/jimp.js
 
