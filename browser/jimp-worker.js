8eb4c4b19dcb7ea8214d161b4e0500ea98a6d307
Phil Seaton <phil@phil-seaton.com>
2015-12-12 10:42:02 -0800

    Some cleanup of the example files index.html and jimp-worker.js

--- browser/jimp-worker.js
+++ browser/jimp-worker.js
@@ -4,38 +4,37 @@ if (!self.Jimp) {
     throw new Error("Could not load jimp.min.js in jimp-worker.js");
 }
 
-//function toArrayBuffer(buffer) {
-//    var ab = new ArrayBuffer(buffer.length);
-//    var view = new Uint8Array(ab);
-//    for (var i = 0; i < buffer.length; ++i) {
-//        view[i] = buffer[i];
-//    }
-//    return ab;
-//}
-
 self.addEventListener('message', function(e) {
-    //var files = e.data;
-    var buffers = [];
+    // Some browsers allow passing of file objects directly from inputs, which would
+    // enable doing the file I/O on the worker thread. Browser support is patchy however,
+    // so the most compatible strategy is to read the file on the main thread asynchronously,
+    // then pass the data here. File I/O is asynchronous on the main thread, and represents
+    // a generally small part of the total image-processing task. 
+    //
+    // See https://developer.mozilla.org/en-US/docs/Web/API/Transferable for support of transferables
+    // Note that passing an array of Transferables makes the worker incompatible with IE10.
+    var arrayBuffer = e.data;
+    if (arrayBuffer.constructor.name !== "ArrayBuffer") {
+        throw new Error("jimp-worker.js expects to be passed a single ArrayBuffer");
+    }
 
-    // Read each file synchronously as an ArrayBuffer and
-    // stash it in a global array to return to the main app.
-    //[].forEach.call(files, function(file) {
-    //    var reader = new FileReaderSync();
-    //    var arrayBuffer = reader.readAsArrayBuffer(file);
+    Jimp.read(arrayBuffer, function (err, image) {
+        if (err) throw err;
 
-// testing mozilla!!
-var arrayBuffer = e.data;
-        Jimp.read(arrayBuffer, function (err, image) {
-            console.log('read complete');
-            if (err) throw err;
-            image.containWithoutBackground(256, 256)            // resize
-                .quality(60)                 // set JPEG quality
-                .greyscale();                 // set greyscale
-            buffers.push(image.bitmap); // includes width and height attributes
+        // Do some image processing in Jimp. This is why we want to use a Web Worker!
+        image.containWithoutBackground(300, 300)            // resize thumbnail
+            .quality(60)                                    // set JPEG quality
+            .greyscale();                                   // set greyscale
+
+        var dataUri = "";
+
+        // Return processed image to main thread
+        // Several data formats included here for example only.
+        // You should only process and return the ones you need!
+        self.postMessage({
+            image: image.bitmap,
+            dataUri: dataUri
         });
-    //});
-
-    console.log("DONE HERE!!!");
-    postMessage(buffers);
+    });
 }, false);
 
