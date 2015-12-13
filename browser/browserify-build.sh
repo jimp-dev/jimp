b6e2b076eb13581ab9cd1cb9acd1ba812f303e69
Phil Seaton <phil@phil-seaton.com>
2015-12-12 22:48:42 -0800

    Moved the build script into the browser dir

--- browser/browserify-build.sh
+++ browser/browserify-build.sh
@@ -5,9 +5,10 @@
 # eg: I/O, Affine Transforms, Bitmap Operations, Gamma Curves, and Layers
 # Initial Build includes only functionality useful for thumbnailing:
 # Resize, (auto)rotate, mime, encoding/decoding jpg/png
+cd ${0%/*}
 echo "Browserifying browser/jimp.js..."
 ENVIRONMENT=BROWSER \
-browserify -t envify -t uglifyify index.js > browser/jimp.js
+browserify -t envify -t uglifyify ../index.js > jimp.js
 echo "Minifying browser/jimp.min.js..."
-uglifyjs browser/jimp.js --compress warnings=false --mangle -o browser/jimp.min.js
+uglifyjs jimp.js --compress warnings=false --mangle -o jimp.min.js
 echo "Done."
