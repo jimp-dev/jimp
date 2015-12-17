33423ebf0c6a6f135ed68c565ba276eace0ed645
Phil Seaton <phil@phil-seaton.com>
2015-12-16 19:17:05 -0800

    Build process now uses babeljs and produces ie10 compatible output in principle. Currently there's an error in my own code, however.

--- browser/browserify-build.sh
+++ browser/browserify-build.sh
@@ -7,10 +7,21 @@
 cd ${0%/*}
 echo "Browserifying browser/jimp.js..."
 ENVIRONMENT=BROWSER \
-browserify -t envify -t uglifyify ../index.js > tmp.js
+browserify -t envify -t uglifyify ../index.js > tmp1.js
+echo "Translating for ES5..."
+babel tmp1.js -o tmp.js --presets es2015,stage-0
+
+
+# A TRUE hack. Use strict at the top seems to cause problems for ie10 interpreting this line from bmp-js:
+# https://github.com/shaozilee/bmp-js/blob/master/lib/decoder.js
+# module.exports = decode = function(bmpData) { ...
+# For some reason, babeljs misses this "error" but IE can parse the code fine without strict mode.
+echo "Removing Strict Mode."
+sed "s/^\"use strict\";//" tmp.js > tmp-nostrict.js
+
 echo "Adding Web Worker wrapper functions..."
-cat tmp.js src/jimp-wrapper.js > lib/jimp.js
+cat tmp-nostrict.js src/jimp-wrapper.js > lib/jimp.js
 echo "Minifying browser/jimp.min.js..."
 uglifyjs lib/jimp.js --compress warnings=false --mangle -o lib/jimp.min.js
 echo "Cleaning up...."
-rm tmp.js
+rm tmp*
