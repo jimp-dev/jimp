6949afa92f8d4237a025c776f9f74679d4990eb3
Oliver Moran <oliver.moran@gmail.com>
2016-04-23 01:13:36 +0100

    add total scripts variable

--- test/tests.sh
+++ test/tests.sh
@@ -11,60 +11,62 @@ function clean {
     rm -r ./output/*.bmp &>/dev/null
 }
 
+TOTALTESTS=14
+
 clean
-echo "promise.js (1/14)"
+echo "promise.js (1/$TOTALTESTS)"
 node promise.js
 
 clean
-echo "callbacks.js (2/14)"
+echo "callbacks.js (2/$TOTALTESTS)"
 node callbacks.js
 
 clean
-echo "chained.js (3/14)"
+echo "chained.js (3/$TOTALTESTS)"
 node chained.js
 
 clean
-echo "create.js (4/14)"
+echo "create.js (4/$TOTALTESTS)"
 node create.js
 
 clean
-echo "rotation.js (5/14)"
+echo "rotation.js (5/$TOTALTESTS)"
 node rotation.js
 
 clean
-echo "filetype.js (6/14)"
+echo "filetype.js (6/$TOTALTESTS)"
 node filetypes.js
 
 clean
-echo "color.js (7/14)"
+echo "color.js (7/$TOTALTESTS)"
 node color.js
 
 clean
-echo "compare.js (8/14)"
+echo "compare.js (8/$TOTALTESTS)"
 node compare.js
 
 clean
-echo "exif.js (9/14)"
+echo "exif.js (9/$TOTALTESTS)"
 node exif.js
 
 clean
-echo "autocrop.js (10/14)"
+echo "autocrop.js (10/$TOTALTESTS)"
 node autocrop.js
 
 clean
-echo "autocrop.js (11/14)"
+echo "autocrop.js (11/$TOTALTESTS)"
 node autocrop.js
 
 clean
-echo "resize.js (13/14)"
+echo "resize.js (13/$TOTALTESTS)"
 node resize.js
 
 clean
-echo "normalize.js (13/14)"
+echo "normalize.js (13/$TOTALTESTS)"
 node normalize.js
 
 clean
-echo "print.js (14/14)"
+echo "print.js (14/$TOTALTESTS)"
 node print.js
 
 clean
