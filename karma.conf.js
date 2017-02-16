155a618dc00434eafbfa9492a6f8c2b6bd9f8f1e
Aurélio A. Heckert <aurium@colivre.coop.br>
2017-02-15 21:10:13 -0300

    View mocha report on karma tester

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,8 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ["mocha-own"],
+        // mochaOwnReporter: { reporter: 'nyan' },
 
         // web server port
         port: 9876,
