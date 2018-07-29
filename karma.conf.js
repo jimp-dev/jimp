b02d505101aa7678ada63dd2b7045e0aa089e280
Andrew Lisowski <lisowski54@gmail.com>
2018-07-28 22:34:48 -0700

    add mocha style reporter back. this one doesn't mess up the stack traces though

--- karma.conf.js
+++ karma.conf.js
@@ -43,6 +43,8 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Firefox', 'Chrome']
+        browsers: ['Firefox', 'Chrome'],
+
+        reporters: ['mocha']
     });
 };
