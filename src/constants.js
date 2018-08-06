92e3ce752c680f481b0505d705c082b0ea966884
Andrew Lisowski <lisowski54@gmail.com>
2018-08-06 00:10:50 -0700

    add constants

--- src/constants.js
+++ src/constants.js
@@ -33,6 +33,18 @@ export const VERTICAL_ALIGN_TOP = 8;
 export const VERTICAL_ALIGN_MIDDLE = 16;
 export const VERTICAL_ALIGN_BOTTOM = 32;
 
+// blend modes
+export const BLEND_SOURCE_OVER = 'srcOver';
+export const BLEND_DESTINATION_OVER = 'dstOver';
+export const BLEND_MULTIPLY = 'multiply';
+export const BLEND_SCREEN = 'screen';
+export const BLEND_OVERLAY = 'overlay';
+export const BLEND_DARKEN = 'darken';
+export const BLEND_LIGHTEN = 'lighten';
+export const BLEND_HARDLIGHT = 'hardLight';
+export const BLEND_DIFFERENCE = 'difference';
+export const BLEND_EXCLUSION = 'exclusion';
+
 // Discover Jimp directory in any environment. (also in fucking karma)
 function getJimpDir() {
     if (process.env.BABEL_ENV === 'test' && process.env.ENV === 'browser') {
