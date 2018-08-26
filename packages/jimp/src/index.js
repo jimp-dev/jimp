a457aa0c3baae96b5cfeb02e87adda04e73acae0
Andrew Lisowski <lisowski54@gmail.com>
2018-08-25 18:40:57 -0700

    add default plugins package

--- packages/jimp/src/index.js
+++ packages/jimp/src/index.js
@@ -1,44 +1,9 @@
 import configure from '@jimp/custom';
 
 import types from '@jimp/types';
-
-import dither from '@jimp/plugin-dither';
-import resize from '@jimp/plugin-resize';
-import blit from '@jimp/plugin-blit';
-import rotate from '@jimp/plugin-rotate';
-import color from '@jimp/plugin-color';
-import print from '@jimp/plugin-print';
-import blur from '@jimp/plugin-blur';
-import crop from '@jimp/plugin-crop';
-import normalize from '@jimp/plugin-normalize';
-import invert from '@jimp/plugin-invert';
-import gaussian from '@jimp/plugin-gaussian';
-import flip from '@jimp/plugin-flip';
-import mask from '@jimp/plugin-mask';
-import scale from '@jimp/plugin-scale';
-import displace from '@jimp/plugin-displace';
-import contain from '@jimp/plugin-contain';
-import cover from '@jimp/plugin-cover';
+import plugins from '@jimp/plugins';
 
 export default configure({
   types: [types],
-  plugins: [
-    dither,
-    resize,
-    blit,
-    rotate,
-    color,
-    print,
-    blur,
-    crop,
-    normalize,
-    invert,
-    gaussian,
-    mask,
-    flip,
-    scale,
-    displace,
-    contain,
-    cover
-  ]
+  plugins: [plugins]
 });
