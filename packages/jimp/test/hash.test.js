4dbb0f21e764874e359b5410f091cf6f67b805f1
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-09-01 01:23:22 -0700

    add test

--- packages/jimp/test/hash.test.js
+++ packages/jimp/test/hash.test.js
@@ -2,9 +2,9 @@
 
 import { Jimp, getTestDir } from './test-helper';
 
-describe('hash', () => {
-  const imagesDir = getTestDir() + '/samples';
+const imagesDir = getTestDir() + '/samples';
 
+describe('hash', () => {
   it('base 2', async () => {
     const image = await Jimp.read(imagesDir + '/dice.png');
 
@@ -46,5 +46,17 @@ describe('hash', () => {
     const image = await Jimp.read(imagesDir + '/lenna.png');
 
     image.hash(17).should.be.equal('4fa6aga5a64ad0c1');
+  });
+});
+
+describe.only('pHash', () => {
+  it('should calculate the distance', async () => {
+    const image1 = await Jimp.read(imagesDir + '/lenna.png');
+    const image2 = await Jimp.read(imagesDir + '/mask.png');
+
+    const hash = image1.pHash();
+    image2
+      .distanceFromHash(hash)
+      .should.be.equal(Jimp.distance(image1, image2));
   });
 });
