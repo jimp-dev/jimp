52def82f6a2d5052f68d4e035d881a419dacce97
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-09-08 22:38:08 -0700

    add diff and distance

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import * as yargs from 'yargs';
-// import * as log from 'log-update';
 import * as logSymbols from 'log-symbols';
 import chalk from 'chalk';
 import Jimp = require('jimp');
@@ -9,8 +8,16 @@ import Jimp = require('jimp');
 const greenCheck = chalk.green(`${logSymbols.success} `);
 const log = (message, verbose = argv && argv.verbose) =>
   verbose && console.log(message);
+const logResult = (functionName, result) =>
+  log(
+    `${greenCheck}  Result of running '${functionName}': ${JSON.stringify(
+      result
+    )}`,
+    true
+  );
 
 const yargsConfig = yargs
+  .scriptName('jimp')
   .option('src', {
     alias: 's',
     describe: 'src file to load into jimp. (PNG, JPEG, TIFF, BMP, or GIF)'
@@ -35,16 +42,87 @@ const yargsConfig = yargs
     describe: 'Path of font to load and be used in text operations'
   })
   .alias('font', 'loadFont')
-  .alias('h', 'help');
+  .alias('h', 'help')
+  .command({
+    command: 'distance [img1] [img2]',
+    describe:
+      'Calculates the hamming distance of two images based on their perceptual hash',
+    builder: function(yargs) {
+      return yargs
+        .example('$0 distance path/to/image.png path/to/another.png', '')
+        .hide('version')
+        .hide('src')
+        .hide('dist')
+        .hide('font')
+        .hide('actions');
+    },
+    handler: async ({ _ }) => {
+      const args = _.slice(1);
+
+      const img1 = await Jimp.read(args[0]);
+      const img2 = await Jimp.read(args[1]);
+      const distance = Jimp.distance(img1, img2);
+
+      logResult('distance', distance);
+    }
+  })
+  .command(
+    'diff',
+    'Diffs two images',
+    function(yargs) {
+      return yargs
+        .example('$0 diff path/to/image.png path/to/another.png -o', '')
+        .option('outputDiff', {
+          alias: 'o',
+          default: 'diff.png',
+          describe: 'File to output diff to.'
+        })
+        .hide('version')
+        .hide('src')
+        .hide('dist')
+        .hide('font')
+        .hide('actions');
+    },
+    async ({ _, outputDiff }) => {
+      const args = _.slice(1);
+
+      const img1 = await Jimp.read(args[0]);
+      const img2 = await Jimp.read(args[1]);
+      const diff = Jimp.diff(img1, img2);
+
+      logResult('diff', diff.percent);
+
+      if (outputDiff) {
+        diff.image.writeAsync(outputDiff === true ? 'diff.png' : outputDiff);
+      }
+    }
+  );
 
 const omitFunctions = [
   'read',
   'create',
   'appendConstructorOption',
-  'distance', // need to make sure it works
-  'diff', // need to make sure it works
-  'loadFont'
+  'distance', // configured above
+  'diff', // configured above
+  'loadFont' // loaded as flag
 ];
+
+const descriptions = {
+  rgbaToInt:
+    'A static helper method that converts RGBA values to a single integer value. args: r, g, b, a (0 - 255)',
+  intToRGBA:
+    'A static helper method that converts RGBA values to a single integer value. args: num (eg 0xFF0000FF)',
+  cssColorToHex:
+    ' Converts a css color (Hex, 8-digit (RGBA) Hex, RGB, RGBA, HSL, HSLA, HSV, HSVA, Named) to a hex number',
+  limit255: 'Limits a number to between 0 or 255. args: num',
+  compareHashes:
+    ' Calculates the hamming distance of two images based on their perceptual hash. args: hash1, hash2',
+  colorDiff:
+    'Compute color difference. args: color1, color2 ({r:val, g:val, b:val, a:val})',
+  measureText: 'Measure how wide printing a string will be. args: text',
+  measureTextHeight:
+    'Measure how tall printing a string will be. args: text, width'
+};
 
 Object.keys(Jimp).map(x => {
   if (omitFunctions.indexOf(x) > -1) {
@@ -55,7 +133,7 @@ Object.keys(Jimp).map(x => {
   if (typeof utilityFunction === 'function') {
     yargsConfig.command(
       x,
-      `Jimp utility function ${x}`,
+      descriptions[x] || `Jimp utility function ${x}`,
       {},
       async ({ _, font }) => {
         const args: any[] = _.slice(1);
@@ -68,9 +146,7 @@ Object.keys(Jimp).map(x => {
         const result = utilityFunction(...args);
 
         if (result !== undefined) {
-          console.log(
-            `${greenCheck}  Result of running '${x}': ${JSON.stringify(result)}`
-          );
+          logResult(x, result);
         }
       }
     );
