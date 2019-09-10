01c6dc0150f0ea5e2499bffb12d099fcd27ef910
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-09-10 16:57:32 -0700

    Added initial 3.1 types

---
+++ packages/jimp/types/ts3.1/index.d.ts
@@ -0,0 +1,29 @@
+/**
+ * While there is nothing in these typings that prevent it from running in TS 2.8 even,
+ * due to the complexity of the typings anything lower than TS 3.1 will only see
+ * Jimp as `any`. In order to test the strict versions of these types in our typing
+ * test suite, the version has been bumped to 3.1
+ */
+
+import {
+  Jimp as JimpType,
+  Bitmap,
+  RGB,
+  RGBA,
+  WellFormedValues,
+  UnionToIntersection,
+  GetPluginVal
+} from '@jimp/core';
+import typeFn from '@jimp/types';
+import pluginFn from '@jimp/plugins';
+
+type Types = ReturnType<typeof typeFn>;
+type Plugins = ReturnType<typeof pluginFn>;
+
+export { Bitmap, RGB, RGBA };
+
+export { FontChar, FontInfo, FontCommon, Font } from '@jimp/plugin-print';
+
+export type FullJimpType = JimpType & UnionToIntersection<GetPluginVal<Types>> & UnionToIntersection<GetPluginVal<Plugins>>;
+declare const Jimp: FullJimpType;
+export default Jimp;
