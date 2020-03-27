b327d64410808e2be428568d217439b8e17e1d08
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-03-27 15:34:03 -0700

    Export the four missing plugins from plugin package

--- packages/plugins/src/index.js
+++ packages/plugins/src/index.js
@@ -1,41 +1,49 @@
 import { mergeDeep } from 'timm';
 
-import dither from '@jimp/plugin-dither';
-import resize from '@jimp/plugin-resize';
 import blit from '@jimp/plugin-blit';
-import rotate from '@jimp/plugin-rotate';
-import color from '@jimp/plugin-color';
-import print from '@jimp/plugin-print';
 import blur from '@jimp/plugin-blur';
-import crop from '@jimp/plugin-crop';
-import normalize from '@jimp/plugin-normalize';
-import invert from '@jimp/plugin-invert';
-import gaussian from '@jimp/plugin-gaussian';
-import flip from '@jimp/plugin-flip';
-import mask from '@jimp/plugin-mask';
-import scale from '@jimp/plugin-scale';
-import displace from '@jimp/plugin-displace';
+import circle from '@jimp/plugin-circle';
+import color from '@jimp/plugin-color';
 import contain from '@jimp/plugin-contain';
 import cover from '@jimp/plugin-cover';
+import crop from '@jimp/plugin-crop';
+import displace from '@jimp/plugin-displace';
+import dither from '@jimp/plugin-dither';
+import fisheye from '@jimp/plugin-fisheye';
+import flip from '@jimp/plugin-flip';
+import gaussian from '@jimp/plugin-gaussian';
+import invert from '@jimp/plugin-invert';
+import mask from '@jimp/plugin-mask';
+import normalize from '@jimp/plugin-normalize';
+import print from '@jimp/plugin-print';
+import resize from '@jimp/plugin-resize';
+import rotate from '@jimp/plugin-rotate';
+import scale from '@jimp/plugin-scale';
+import shadow from '@jimp/plugin-shadow';
+import threshold from '@jimp/plugin-threshold';
 
 const plugins = [
-  dither,
-  resize,
   blit,
-  rotate,
-  color,
-  print,
   blur,
-  crop,
-  normalize,
-  invert,
-  gaussian,
-  flip,
-  mask,
-  scale,
-  displace,
+  circle,
+  color,
   contain,
-  cover
+  cover,
+  crop,
+  displace,
+  dither,
+  fisheye,
+  flip,
+  gaussian,
+  invert,
+  mask,
+  normalize,
+  print,
+  resize,
+  rotate,
+  scale,
+  shadow,
+  threshold
 ];
 
 export default jimpEvChange => {
