bb50bf98ae71944029e3f86d02dd879fee0b382b
Emily Marigold Klassen <forivall@gmail.com>
2020-04-10 20:38:32 -0700

    doc: improve typings

--- packages/utils/index.d.ts
+++ packages/utils/index.d.ts
@@ -6,4 +6,10 @@ export function isNodePattern(cb: Omit<any, Function>): false;
 
 export function throwError(error: string | Error, cb?: (err: Error) => void): ThrowStatement;
 
-export function scan(image: Image, x: number, y: number, w: number, h: number, f: (image: Image, _x: number, _y: number, idx: number) => void): Image;
+export function scan<I extends Image>(image: I, x: number, y: number, w: number, h: number, f: (image: I, _x: number, _y: number, idx: number) => void): I;
+export function* scanIterator<I extends I>(image: I, x: number, y: number, w: number, h: number): IterableIterator<{
+  image: I;
+  x: number;
+  y: number;
+  idx: number;
+}>;
