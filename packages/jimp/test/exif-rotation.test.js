981a64939c64aebfe0a2a9bee7b02bf87d0d0092
Juan Pablo Garcia Ripa <sarabadu@gmail.com>
2023-06-24 00:53:04 +0200

    reset exifOrientation
    Signed-off-by: Juan Pablo Garcia Ripa <sarabadu@gmail.com>

--- packages/jimp/test/exif-rotation.test.js
+++ packages/jimp/test/exif-rotation.test.js
@@ -1,6 +1,7 @@
 import { Jimp, getTestDir } from "@jimp/test-utils";
 import expect from "@storybook/expect";
 
+import { getExifOrientation } from "@jimp/core/src/utils/image-bitmap";
 import configure from "@jimp/custom";
 
 const jimp = configure({ plugins: [] }, Jimp);
@@ -11,15 +12,45 @@ describe("EXIF orientation", () => {
       const regularImg = await imageWithOrientation(1);
       const orientedImg = await imageWithOrientation(orientation);
 
+      orientedImg.write(
+        getTestDir(__dirname) + `/Landscape-output-${orientation}-PR.jpg`
+      );
       expect(orientedImg.getWidth()).toBe(regularImg.getWidth());
       expect(orientedImg.getHeight()).toBe(regularImg.getHeight());
       expect(Jimp.distance(regularImg, orientedImg)).toBeLessThan(0.07);
+
+      expect(getExifOrientation(orientedImg)).toBe(
+        getExifOrientation(regularImg)
+      );
+    });
+  }
+
+  for (let orientation = 1; orientation <= 8; orientation++) {
+    it(`is fixed when EXIF orientation is ${orientation}`, async () => {
+      const regularImg = await imageWithOrientation(1);
+      const orientedImg = await imageWithOrientation(orientation);
+
+      orientedImg.write(
+        getTestDir(__dirname) + `/Portrait-output-${orientation}-PR.jpg`
+      );
+      expect(orientedImg.getWidth()).toBe(regularImg.getWidth());
+      expect(orientedImg.getHeight()).toBe(regularImg.getHeight());
+      expect(Jimp.distance(regularImg, orientedImg)).toBeLessThan(0.07);
+
+      expect(getExifOrientation(orientedImg)).toBe(
+        getExifOrientation(regularImg)
+      );
     });
   }
 });
 
 function imageWithOrientation(orientation) {
   const imageName = `Landscape_${orientation}.jpg`;
+  const path = getTestDir(__dirname) + "/images/exif-orientation/" + imageName;
+  return jimp.read(path);
+}
+function imageWithOrientation2(orientation) {
+  const imageName = `Portrait_${orientation}.jpg`;
   const path = getTestDir(__dirname) + "/images/exif-orientation/" + imageName;
   return jimp.read(path);
 }
