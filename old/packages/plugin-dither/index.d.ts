4a6a79c634e641cc3c9eea6cbf43d86bed4abeb2
Andrew Lisowski <lisowski54@gmail.com>
2024-03-24 23:59:41 -0700

    move dither

--- old/packages/plugin-dither/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ImageCallback } from "@jimp/core";
-
-interface Dither {
-  dither565(cb?: ImageCallback<this>): this;
-  dither16(cb?: ImageCallback<this>): this;
-}
-
-export default function (): Dither;
---
+++ plugins/plugin-dither/src/index.ts
@@ -0,0 +1,29 @@
+import { JimpClass } from "@jimp/types";
+
+/**
+ * Apply a ordered dithering effect
+ */
+export function dither<I extends JimpClass>(image: I) {
+  const rgb565Matrix = [1, 9, 3, 11, 13, 5, 15, 7, 4, 12, 2, 10, 16, 8, 14, 6];
+
+  image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+    const thresholdId = ((y & 3) << 2) + (x % 4);
+    const dither = rgb565Matrix[thresholdId]!;
+
+    image.bitmap.data[idx] = Math.min(image.bitmap.data[idx]! + dither, 0xff);
+    image.bitmap.data[idx + 1] = Math.min(
+      image.bitmap.data[idx + 1]! + dither,
+      0xff
+    );
+    image.bitmap.data[idx + 2] = Math.min(
+      image.bitmap.data[idx + 2]! + dither,
+      0xff
+    );
+  });
+
+  return image;
+}
+
+export default () => ({
+  dither,
+});
