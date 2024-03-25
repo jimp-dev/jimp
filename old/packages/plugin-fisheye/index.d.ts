e78e802969bb48d8ef8ec14a202800e1df522ee5
Andrew Lisowski <lisowski54@gmail.com>
2024-03-25 10:16:06 -0700

    plugin fisheye

--- old/packages/plugin-fisheye/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ImageCallback } from "@jimp/core";
-
-interface Fisheye {
-  fishEye(opts?: { r: number }, cb?: ImageCallback<this>): this;
-  fishEye(cb?: ImageCallback<this>): this;
-}
-
-export default function (): Fisheye;
---
+++ plugins/plugin-fisheye/src/index.ts
@@ -0,0 +1,39 @@
+import { JimpClass } from "@jimp/types";
+import { clone } from "@jimp/utils";
+
+export function fisheye<I extends JimpClass>(image: I, { r = 2.5 } = {}) {
+  const source = clone(image);
+  const { width, height } = source.bitmap;
+
+  source.scan((x, y) => {
+    const hx = x / width;
+    const hy = y / height;
+    const rActual = Math.sqrt(Math.pow(hx - 0.5, 2) + Math.pow(hy - 0.5, 2));
+    const rn = 2 * Math.pow(rActual, r);
+    const cosA = (hx - 0.5) / rActual;
+    const sinA = (hy - 0.5) / rActual;
+    const newX = Math.round((rn * cosA + 0.5) * width);
+    const newY = Math.round((rn * sinA + 0.5) * height);
+    const color = source.getPixelColor(newX, newY);
+
+    image.setPixelColor(color, x, y);
+  });
+
+  /* Set center pixel color, otherwise it will be transparent */
+  image.setPixelColor(
+    source.getPixelColor(width / 2, height / 2),
+    width / 2,
+    height / 2
+  );
+
+  return image;
+}
+
+export default function fisheyePlugin() {
+  return {
+    /**
+     *  Adds a fisheye effect to the image
+     */
+    fisheye,
+  };
+}
