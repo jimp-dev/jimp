39195eec7a70f536c8ea7ccd8cb65448db80d25b
Andrew Lisowski <lisowski54@gmail.com>
2024-03-25 17:13:17 -0700

    plugin cover

--- plugins/plugin-cover/src/index.ts
+++ plugins/plugin-cover/src/index.ts
@@ -1,10 +1,9 @@
 import { JimpClass } from "@jimp/types";
-import { HorizontalAlign, VerticalAlign } from "@jimp/core";
-import { clone } from "@jimp/utils";
-import { ResizeStrategy, resize, scale } from "@jimp/plugin-resize";
-import { blit } from "@jimp/plugin-blit";
+import { VerticalAlign, HorizontalAlign } from "@jimp/core";
+import { ResizeStrategy, scale } from "@jimp/plugin-resize";
+import { crop } from "@jimp/plugin-crop";
 
-function contain<I extends JimpClass>(
+export function cover<I extends JimpClass>(
   image: I,
   w: number,
   h: number,
@@ -12,7 +11,6 @@ function contain<I extends JimpClass>(
   mode?: ResizeStrategy
 ) {
   alignBits = alignBits || HorizontalAlign.CENTER | VerticalAlign.MIDDLE;
-
   const hbits = alignBits & ((1 << 3) - 1);
   const vbits = alignBits >> 3;
 
@@ -31,33 +29,28 @@ function contain<I extends JimpClass>(
 
   const f =
     w / h > image.bitmap.width / image.bitmap.height
-      ? h / image.bitmap.height
-      : w / image.bitmap.width;
+      ? w / image.bitmap.width
+      : h / image.bitmap.height;
 
-  const c = scale(clone(image), f, mode);
-
-  image = resize(image, w, h, mode);
-
-  image.scan((_, __, idx) => {
-    image.bitmap.data.writeUInt32BE(image.background, idx);
-  });
-
-  image = blit(image, {
-    src: c,
-    x: ((image.bitmap.width - c.bitmap.width) / 2) * alignH,
-    y: ((image.bitmap.height - c.bitmap.height) / 2) * alignV,
-  });
+  image = scale(image, f, mode);
+  image = crop(
+    image,
+    ((image.bitmap.width - w) / 2) * alignH,
+    ((image.bitmap.height - h) / 2) * alignV,
+    w,
+    h
+  );
 
   return image;
 }
 
 export default () => ({
   /**
-   * Scale the image to the given width and height keeping the aspect ratio. Some parts of the image may be letter boxed.
+   * Scale the image so the given width and height keeping the aspect ratio. Some parts of the image may be clipped.
    * @param w the width to resize the image to
    * @param h the height to resize the image to
    * @param alignBits A bitmask for horizontal and vertical alignment
    * @param mode a scaling method (e.g. Jimp.RESIZE_BEZIER)
    */
-  contain,
+  cover,
 });
