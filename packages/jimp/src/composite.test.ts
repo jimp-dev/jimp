1a3e024ad3f148a997d9a7c2c63b3582e4d2096a
Andrew Lisowski <lisowski54@gmail.com>
2024-03-26 16:03:53 -0700

    move composite tests

---
+++ packages/jimp/src/composite.test.ts
@@ -0,0 +1,41 @@
+import { expect, test, describe } from "vitest";
+import fs from "fs";
+
+import { getTestImagePath } from "@jimp/test-utils";
+
+import { Jimp, BlendMode } from "./index.js";
+
+// TODO: Figure out why we need to write to file to get equal buffers
+describe("composite", () => {
+  test("can apply more than one color transformation", async () => {
+    const mask = new Jimp({ height: 100, width: 100, color: 0x0000ff });
+    const cops = await Jimp.fromBuffer(
+      fs.readFileSync(getTestImagePath("cops.jpg"))
+    );
+
+    cops.composite(mask, 0, 0, {
+      mode: BlendMode.SRC_OVER,
+      opacitySource: 0.5,
+      opacityDest: 0.5,
+    });
+
+    const output = await cops.getBuffer("image/png");
+    expect(output).toMatchImageSnapshot();
+  });
+
+  test("should handle edges correctly", async () => {
+    const background = new Jimp({ height: 100, width: 100, color: 0x0000ff });
+    const cops = await Jimp.fromBuffer(
+      fs.readFileSync(getTestImagePath("cops.jpg"))
+    );
+
+    background.composite(cops, 0, -(cops.bitmap.height / 2), {
+      mode: BlendMode.SRC_OVER,
+      opacityDest: 1,
+      opacitySource: 0.8,
+    });
+
+    const output = await background.getBuffer("image/png");
+    expect(output).toMatchImageSnapshot();
+  });
+});
