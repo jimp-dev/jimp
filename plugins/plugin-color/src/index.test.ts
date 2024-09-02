dcfd6abeabb4658dcda7e37f839b3dac2753dd15
Andrew Lisowski <lisowski54@gmail.com>
2024-09-01 21:09:16 -0700

    Make brightness function behave like other implementations

--- plugins/plugin-color/src/index.test.ts
+++ plugins/plugin-color/src/index.test.ts
@@ -37,3 +37,27 @@ describe("canvas color transformation", () => {
     expect(image.color([{ apply: "hue", params: [150] }])).toMatchSnapshot();
   });
 });
+
+describe("brightness", () => {
+  test("brightness of 1 should be the same as no change", () => {
+    const jgd = makeDonutTestImage(0xff000088, 0xff000088, 0xff000088);
+    const image = jimp.fromBitmap(jgd);
+    const image2 = jimp.fromBitmap(jgd);
+
+    expect(image.brightness(1).bitmap).toStrictEqual(image2.bitmap);
+  });
+
+  test("should be able to brighten", () => {
+    const jgd = makeDonutTestImage(0xaa000088, 0xaa000088, 0xaa000088);
+    const image = jimp.fromBitmap(jgd);
+
+    expect(image.brightness(2).bitmap).toMatchSnapshot();
+  });
+
+  test("should be able to darken", () => {
+    const jgd = makeDonutTestImage(0xaa000088, 0xaa000088, 0xaa000088);
+    const image = jimp.fromBitmap(jgd);
+
+    expect(image.brightness(0.2).bitmap).toMatchSnapshot();
+  });
+});
