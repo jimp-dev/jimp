fb30d6be6853b0bf5ae9312bff49b7258546ed80
Andrew Lisowski <lisowski54@gmail.com>
2024-09-02 23:38:29 -0700

    add png optimization

--- plugins/wasm-png/README.md
+++ plugins/wasm-png/README.md
@@ -1,6 +1,7 @@
 # `@jimp/wasm-png`
 
 A format plugin for Jimp that adds support for PNG images using the [rust crate](https://docs.rs/png/0.11.0/png/).
+It also support optimizing the image using [oxipng](https://github.com/shssoichiro/oxipng).
 
 > NOTE: Only works in esm environments.
 
--- plugins/wasm-png/src/index.ts
+++ plugins/wasm-png/src/index.ts
@@ -1,22 +1,41 @@
 import decode, { init as initDecoder } from "@jsquash/png/decode.js";
 import encode, { init as initEncoder } from "@jsquash/png/encode.js";
 import { Format } from "@jimp/types";
+import { optimise } from "@jsquash/oxipng";
+import z from "zod";
 
-export type PngOptions = {
-  colorSpace?: "display-p3" | "srgb";
-};
+const PngOptionsSchema = z.object({
+  colorSpace: z.union([z.literal("display-p3"), z.literal("srgb")]).optional(),
+  optimize: z
+    .object({
+      /** whether to use PNG interlacing or not. Interlacing will increase the size of an optimised image. */
+      interlace: z.boolean().optional().default(false),
+      /** is the optimisation level between 1 to 6. The higher the level, the higher the compression. Any level above 4 is not recommended. */
+      level: z.number().min(0).max(6).optional().default(2),
+      /** whether to allow transparent pixels to be altered to improve compression. */
+      optimiseAlpha: z.boolean().optional().default(false),
+    })
+    .optional(),
+});
+
+type PngOptions = z.infer<typeof PngOptionsSchema>;
 
 export default function png() {
   return {
     mime: "image/png",
     hasAlpha: true,
-    encode: async (bitmap, { colorSpace = "srgb" }: PngOptions = {}) => {
+    encode: async (bitmap, options: Partial<PngOptions> = {}) => {
+      const { colorSpace = "srgb", optimize } = PngOptionsSchema.parse(options);
       await initEncoder();
-      const arrayBuffer = await encode({
+      let arrayBuffer = await encode({
         ...bitmap,
         data: new Uint8ClampedArray(bitmap.data),
         colorSpace,
       });
+
+      if (optimize) {
+        arrayBuffer = await optimise(arrayBuffer, optimize);
+      }
 
       return Buffer.from(arrayBuffer);
     },
